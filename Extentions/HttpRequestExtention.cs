c0ba03bfb35991f8eda3b6b9e6cdf68598d23f26
Chaleshka <52744003+Chaleshka@users.noreply.github.com>
2025-07-25 11:52:10 +0300

    Move extentions from / to /EXtentions

---
+++ Extentions/HttpRequestExtention.cs
@@ -0,0 +1,21 @@
+﻿using ModuWeb.Json;
+using System.Net.NetworkInformation;
+using static ModuWeb.examples.WhatsYourNameModule;
+
+namespace ModuWeb.Extentions
+{
+    public static class HttpRequestExtention
+    {
+        public async static Task<T?> GetRequestData<T>(this HttpRequest request) where T : new()
+        {
+            if (request.Method.ToUpper() == "GET")
+                return QueryParser.Parse<T?>(request.Query);
+            else
+            {
+                using var reader = new StreamReader(request.Body);
+                string rawJson = await reader.ReadToEndAsync();
+                return CustomJsonDeserializer.Deserialize<T?>(rawJson);
+            }
+        }
+    }
+}
